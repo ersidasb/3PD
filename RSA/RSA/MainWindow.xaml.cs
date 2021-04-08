51c29477b77dbc6e2843c79d4fcf2ea3d1cdb757
Ersidas Baniulis <ersidasb@gmail.com>
2021-04-08 16:35:25 +0300

    Made a simple UI
    Added textblocks, texboxes, radio buttons and a button
    Added a updateButton() method which checks if all fields are filled

--- RSA/RSA/MainWindow.xaml.cs
+++ RSA/RSA/MainWindow.xaml.cs
@@ -24,5 +24,43 @@ namespace RSA
         {
             InitializeComponent();
         }
+
+        private void btnCalculate_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void rBtnEncrypt_Checked(object sender, RoutedEventArgs e)
+        {
+            updateButton();
+        }
+
+        private void rBtnDecrypt_Checked(object sender, RoutedEventArgs e)
+        {
+            updateButton();
+        }
+
+        private void tBxInputText_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            updateButton();
+        }
+
+        private void tBxInputP_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            updateButton();
+        }
+
+        private void tBxInputQ_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            updateButton();
+        }
+        
+        private void updateButton()
+        {
+            if (tBxInputText.Equals("") || tBxInputP.Equals("") || tBxInputQ.Equals("") || rBtnEncrypt.IsChecked == false && rBtnDecrypt.IsChecked == false)
+                btnCalculate.IsEnabled = false;
+            else
+                btnCalculate.IsEnabled = true;
+        }
     }
 }
